2c8e96d61fa4c79e690d90f326756bd8fc816c06
David Turner <dturner@twopensource.com>
2016-07-03 03:58:07 -0400

    index-helper: kill mode
    Add a new command (and command-line arg) to allow index-helpers to
    exit cleanly.
    
    This is mainly useful for tests.
    
    Signed-off-by: David Turner <dturner@twopensource.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- index-helper.c
+++ index-helper.c
@@ -336,6 +336,8 @@ static void loop(int fd, int idle_in_seconds)
 					 * alive, nothing to do.
 					 */
 				}
+			} else if (!strcmp(buf, "die")) {
+				break;
 			} else {
 				warning("BUG: Bogus command %s", buf);
 			}
@@ -357,10 +359,29 @@ static const char * const usage_text[] = {
 	NULL
 };
 
+static void request_kill(void)
+{
+	int fd = unix_stream_connect(git_path("index-helper.sock"));
+
+	if (fd >= 0) {
+		write_in_full(fd, "die", 4);
+		close(fd);
+	}
+
+	/*
+	 * The child will try to do this anyway, but we want to be
+	 * ready to launch a new daemon immediately after this command
+	 * returns.
+	 */
+
+	unlink(git_path("index-helper.sock"));
+	return;
+}
+
 int main(int argc, char **argv)
 {
 	const char *prefix;
-	int idle_in_seconds = 600, detach = 0;
+	int idle_in_seconds = 600, detach = 0, kill = 0;
 	int fd;
 	struct strbuf socket_path = STRBUF_INIT;
 	struct option options[] = {
@@ -369,6 +390,7 @@ int main(int argc, char **argv)
 		OPT_BOOL(0, "strict", &to_verify,
 			 N_("verify shared memory after creating")),
 		OPT_BOOL(0, "detach", &detach, N_("detach the process")),
+		OPT_BOOL(0, "kill", &kill, N_("request that existing index helper processes exit")),
 		OPT_END()
 	};
 
@@ -382,6 +404,13 @@ int main(int argc, char **argv)
 	if (parse_options(argc, (const char **)argv, prefix,
 			  options, usage_text, 0))
 		die(_("too many arguments"));
+
+	if (kill) {
+		if (detach)
+			die(_("--kill doesn't want any other options"));
+		request_kill();
+		return 0;
+	}
 
 	atexit(cleanup);
 	sigchain_push_common(cleanup_on_signal);
