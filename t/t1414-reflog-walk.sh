beafb2c62947a6d4a97b9c3baf99fe62ec8e830f
Jeff King <peff@peff.net>
2017-07-05 04:09:04 -0400

    reflog-walk: stop using fake parents
    The reflog-walk system works by putting a ref's tip into the
    pending queue, and then "traversing" the reflog by
    pretending that the parent of each commit is the previous
    reflog entry.
    
    This causes a number of user-visible oddities, as documented
    in t1414 (and the commit message which introduced it). We
    can fix all of them in one go by replacing the fake-reflog
    system with a much simpler one: just keeping a list of
    reflogs to show, and walking through them entry by entry.
    
    The implementation is fairly straight-forward, but there are
    a few items to note:
    
      1. We obviously must skip calling add_parents_to_list()
         when we are traversing reflogs, since we do not want to
         walk the original parents at all.
    
         As a result, we must call try_to_simplify_commit()
         ourselves and skip any TREESAME commits.
    
         There are other parts of add_parents_to_list() we skip,
         as well, but none of them should matter for a reflog
         traversal:
    
           -  We do not allow UNINTERESTING commits, nor
              symmetric ranges (and we bail when these are used
              with "-g").
    
           - Using --source makes no sense, since we aren't
             traversing. The reflog selector shows the same
             information with more detail.
    
           - Using --first-parent is still sensible, since you
             may want to see the first-parent diff for each
             entry. But since we're not traversing, we don't
             need to cull the parent list here.
    
      2. Since we now just walk the reflog entries themselves,
         rather than starting with the ref tip, we now look at
         the "new" field of each entry rather than the "old"
         (i.e., we are showing entries, not faking parents).
         This removes all of the tricky logic around skipping
         past root commits.
    
         But note that we have no way to show an entry with the
         null sha1 in its "new" field (because such a commit
         obviously does not exist). Normally this would not
         happen, since we delete reflogs along with refs, but
         there is one special case. When we rename the currently
         checked out branch, we write two reflog entries into
         the HEAD log: one where the commit goes away, and
         another where it comes back.
    
         Prior to this commit, we show both entries with
         identical reflog messages. After this commit, we show
         only the "comes back" entry. See the update in t3200
         which demonstrates this.
    
         Arguably either is fine, as the whole double-entry
         thing is a bit hacky in the first place. And until a
         recent fix, we truncated the traversal in such a case
         anyway, which was _definitely_ wrong.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1414-reflog-walk.sh
+++ t/t1414-reflog-walk.sh
@@ -34,32 +34,32 @@ test_expect_success 'reflog walk shows expected logs' '
 	test_cmp expect.all actual
 '
 
-test_expect_failure 'reflog can limit with --no-merges' '
+test_expect_success 'reflog can limit with --no-merges' '
 	grep -v merge expect.all >expect &&
 	do_walk --no-merges >actual &&
 	test_cmp expect actual
 '
 
-test_expect_failure 'reflog can limit with pathspecs' '
+test_expect_success 'reflog can limit with pathspecs' '
 	grep two expect.all >expect &&
 	do_walk -- two.t >actual &&
 	test_cmp expect actual
 '
 
-test_expect_failure 'pathspec limiting handles merges' '
+test_expect_success 'pathspec limiting handles merges' '
 	sed -n "1p;3p;5p" expect.all >expect &&
 	do_walk -- one.t >actual &&
 	test_cmp expect actual
 '
 
-test_expect_failure '--parents shows true parents' '
+test_expect_success '--parents shows true parents' '
 	# convert newlines to spaces
 	echo $(git rev-parse HEAD HEAD^1 HEAD^2) >expect &&
 	git rev-list -g --parents -1 HEAD >actual &&
 	test_cmp expect actual
 '
 
-test_expect_failure 'walking multiple reflogs shows both' '
+test_expect_success 'walking multiple reflogs shows both' '
 	{
 		do_walk HEAD &&
 		do_walk side
@@ -68,7 +68,7 @@ test_expect_failure 'walking multiple reflogs shows both' '
 	test_cmp expect actual
 '
 
-test_expect_failure 'walk prefers reflog to ref tip' '
+test_expect_success 'walk prefers reflog to ref tip' '
 	head=$(git rev-parse HEAD) &&
 	one=$(git rev-parse one) &&
 	ident="$GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> $GIT_COMMITTER_DATE" &&
