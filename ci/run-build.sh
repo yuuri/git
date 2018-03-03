b1d1f64bce1eceb844f9c40e8ae43953df404d5c
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2018-03-03 08:46:05 +0700

    travis-ci: enable more warnings on travis linux-gcc job
    We have DEVELOPER config to enable more warnings, but since we can't set
    a fixed gcc version to all devs, that has to be a bit more conservative.
    On travis, we know almost exact version to be used (bumped to 6.x for
    more warnings), we could be more aggressive.
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/run-build.sh
+++ ci/run-build.sh
@@ -5,4 +5,19 @@
 
 . ${0%/*}/lib-travisci.sh
 
+if [ "$jobname" = linux-gcc ]; then
+	gcc-6 --version
+	cat >config.mak <<-EOF
+	CC=gcc-6
+	CFLAGS = -g -O2 -Wall
+	CFLAGS += -Wextra
+	CFLAGS += -Wmissing-prototypes
+	CFLAGS += -Wno-empty-body
+	CFLAGS += -Wno-maybe-uninitialized
+	CFLAGS += -Wno-missing-field-initializers
+	CFLAGS += -Wno-sign-compare
+	CFLAGS += -Wno-unused-function
+	CFLAGS += -Wno-unused-parameter
+	EOF
+fi
 make --jobs=2
