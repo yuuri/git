21b2f9e4b90a7e25ced8ce561a22aae1a419401a
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-05-03 03:01:00 +0200

    range-diff: offer to dual-color the diffs
    When showing what changed between old and new commits, we show a diff of
    the patches. This diff is a diff between diffs, therefore there are
    nested +/- signs, and it can be relatively hard to understand what is
    going on.
    
    With the --dual-color option, the preimage and the postimage are colored
    like the diffs they are, and the *outer* +/- sign is inverted for
    clarity.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- builtin/range-diff.c
+++ builtin/range-diff.c
@@ -20,9 +20,12 @@ int cmd_range_diff(int argc, const char **argv, const char *prefix)
 {
 	int creation_factor = 60;
 	struct diff_options diffopt = { NULL };
+	int dual_color = 0;
 	struct option options[] = {
 		OPT_INTEGER(0, "creation-factor", &creation_factor,
 			    N_("Percentage by which creation is weighted")),
+		OPT_BOOL(0, "dual-color", &dual_color,
+			    N_("color both diff and diff-between-diffs")),
 		OPT_END()
 	};
 	int i, j, res = 0;
@@ -51,6 +54,11 @@ int cmd_range_diff(int argc, const char **argv, const char *prefix)
 	}
 	argc = j;
 	diff_setup_done(&diffopt);
+
+	if (dual_color) {
+		diffopt.use_color = 1;
+		diffopt.flags.dual_color_diffed_diffs = 1;
+	}
 
 	if (argc == 2) {
 		if (!strstr(argv[0], ".."))
