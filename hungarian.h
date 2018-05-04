bf48bf9c29d6ea1447a456337f40138cde56b190
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-05-04 17:34:29 +0200

    Add a function to solve least-cost assignment problems
    The Jonker-Volgenant algorithm was implemented to answer questions such
    as: given two different versions of a topic branch (or iterations of a
    patch series), what is the best pairing of commits/patches between the
    different versions?
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ hungarian.h
@@ -0,0 +1,19 @@
+#ifndef HUNGARIAN_H
+#define HUNGARIAN_H
+
+/*
+ * Compute an assignment of columns -> rows (and vice versa) such that every
+ * column is assigned to at most one row (and vice versa) minimizing the
+ * overall cost.
+ *
+ * The parameter `cost` is the cost matrix: the cost to assign column j to row
+ * i is `cost[j + column_count * i].
+ *
+ * The arrays column2row and row2column will be populated with the respective
+ * assignments (-1 for unassigned, which can happen only if column_count !=
+ * row_count).
+ */
+int compute_assignment(int column_count, int row_count, double *cost,
+		       int *column2row, int *row2column);
+
+#endif
