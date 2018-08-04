7f92d92154143127734a638e41e064adce46a2e2
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-04 22:56:05 +0200

    line-log: adjust start/end of ranges individually
    When traversing commits and adjusting the ranges, things can get really
    tricky. For example, when the line range of interest encloses several
    hunks of a commit, the line range can actually shrink.
    
    Currently, range_set_shift_diff() does not anticipate that scenario and
    blindly adjusts start and end by the same offset ("shift" the range).
    
    This can lead to a couple of surprising issues, such as assertions in
    range_set_append() (when the end of a given range is not adjusted
    properly, it can point after the start of the next range) or even
    segmentation faults (when t_end in the loop of dump_diff_hacky_one()
    points outside the valid line range).
    
    Let's fix this by adjusting the start and the end offsets individually.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- line-log.c
+++ line-log.c
@@ -427,7 +427,7 @@ static void range_set_shift_diff(struct range_set *out,
 				 struct diff_ranges *diff)
 {
 	unsigned int i, j = 0;
-	long offset = 0;
+	long offset = 0, start_offset;
 	struct range *src = rs->ranges;
 	struct range *target = diff->target.ranges;
 	struct range *parent = diff->parent.ranges;
@@ -438,7 +438,13 @@ static void range_set_shift_diff(struct range_set *out,
 				- (target[j].end-target[j].start);
 			j++;
 		}
-		range_set_append(out, src[i].start+offset, src[i].end+offset);
+		start_offset = offset;
+		while (j < diff->target.nr && src[i].end > target[j].end) {
+			offset += (parent[j].end-parent[j].start)
+				- (target[j].end-target[j].start);
+			j++;
+		}
+		range_set_append(out, src[i].start+start_offset, src[i].end+offset);
 	}
 }
 
