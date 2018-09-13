78f7796f3b9e91f4ec357cc4da5218d2d3fedd5f
Ben Peart <benpeart@microsoft.com>
2018-09-13 17:45:42 +0000

    fsmonitor: update GIT_TEST_FSMONITOR support
    Rename GIT_FSMONITOR_TEST to GIT_TEST_FSMONITOR for consistency with the
    other GIT_TEST_ special setups and properly document its use.
    
    Signed-off-by: Ben Peart <Ben.Peart@microsoft.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7519-status-fsmonitor.sh
+++ t/t7519-status-fsmonitor.sh
@@ -8,7 +8,7 @@ test_description='git status with file system watcher'
 # To run the entire git test suite using fsmonitor:
 #
 # copy t/t7519/fsmonitor-all to a location in your path and then set
-# GIT_FSMONITOR_TEST=fsmonitor-all and run your tests.
+# GIT_TEST_FSMONITOR=fsmonitor-all and run your tests.
 #
 
 # Note, after "git reset --hard HEAD" no extensions exist other than 'TREE'
