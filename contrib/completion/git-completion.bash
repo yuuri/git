830191fbfbf95b9d568ba91ddcdd9860173a476a
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-11-16 13:30:44 +0100

    completion: complete <rev> in `git worktree add <path> <rev>`
    The second non-option argument to `git worktree`'s `add` command is an
    optional revision. Let's complete it.
    
    Inspired by https://github.com/git-for-windows/git/pull/1681.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- contrib/completion/git-completion.bash
+++ contrib/completion/git-completion.bash
@@ -2773,6 +2773,11 @@ _git_worktree ()
 		add,--*)
 			__gitcomp_builtin worktree_add
 			;;
+		add,*)
+			if [ $(__git_count_arguments "worktree") -eq 2 ]; then
+				__git_complete_refs
+			fi
+			;;
 		list,--*)
 			__gitcomp_builtin worktree_list
 			;;
