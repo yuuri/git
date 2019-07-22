49be7a73454b13b04270531eece87a2c6f6d3189
Derrick Stolee <dstolee@microsoft.com>
2019-07-22 08:34:28 -0400

    repo-settings: parse core.untrackedCache
    The core.untrackedCache config setting is slightly complicated,
    so clarify its use and centralize its parsing into the repo
    settings.
    
    The default value is "keep" (returned as -1), which persists the
    untracked cache if it exists.
    
    If the value is set as "false" (returned as 0), then remove the
    untracked cache if it exists.
    
    If the value is set as "true" (returned as 1), then write the
    untracked cache and persist it.
    
    Instead of relying on magic values of -1, 0, and 1, split these
    options into an enum. This allows the use of "-1" as a
    default value. After parsing the config options, if the value is
    unset we can initialize it to UNTRACKED_CACHE_KEEP.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- repo-settings.h
+++ repo-settings.h
@@ -1,11 +1,19 @@
 #ifndef REPO_SETTINGS_H
 #define REPO_SETTINGS_H
 
+enum untracked_cache_setting {
+	UNTRACKED_CACHE_UNSET = -1,
+	UNTRACKED_CACHE_REMOVE = 0,
+	UNTRACKED_CACHE_KEEP = 1,
+	UNTRACKED_CACHE_WRITE = 2
+};
+
 struct repo_settings {
 	int core_commit_graph;
 	int gc_write_commit_graph;
 
 	int index_version;
+	enum untracked_cache_setting core_untracked_cache;
 
 	int pack_use_sparse;
 };
