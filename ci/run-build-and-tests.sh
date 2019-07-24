0ab9e473b4cb2031bf1a5b56c398566705cb3bec
Josh Steadmon <steadmon@google.com>
2019-07-24 16:06:53 -0700

    ci: run trace2 schema validation in the CI suite
    Signed-off-by: Josh Steadmon <steadmon@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- ci/run-build-and-tests.sh
+++ ci/run-build-and-tests.sh
@@ -14,6 +14,8 @@ make
 make test
 if test "$jobname" = "linux-gcc"
 then
+	make -C t/trace_schema_validator
+	export GIT_TRACE2_EVENT=$(mktemp)
 	export GIT_TEST_SPLIT_INDEX=yes
 	export GIT_TEST_FULL_IN_PACK_ARRAY=true
 	export GIT_TEST_OE_SIZE=10
@@ -21,6 +23,10 @@ then
 	export GIT_TEST_COMMIT_GRAPH=1
 	export GIT_TEST_MULTI_PACK_INDEX=1
 	make test
+	t/trace_schema_validator/trace_schema_validator \
+		--trace2_event_file=${GIT_TRACE2_EVENT} \
+		--schema_file=t/trace_schema_validator/strict_schema.json \
+		--progress=10000
 fi
 
 check_unignored_build_artifacts
