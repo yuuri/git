ca59b118f1fa4176214f55b8993145b5e1db39a0
Derrick Stolee <dstolee@microsoft.com>
2019-10-22 10:15:50 -0400

    commit-graph: fix writing first commit-graph during fetch
    The previous commit includes a failing test for an issue around
    fetch.writeCommitGraph and fetching in a repo with a submodule. Here, we
    fix that bug and set the test to "test_expect_success".
    
    The prolem arises with this set of commands when the remote repo at
    <url> has a submodule. Note that --recurse-submodules is not needed to
    demonstrate the bug.
    
    	$ git clone <url> test
    	$ cd test
    	$ git -c fetch.writeCommitGraph=true fetch origin
    	Computing commit graph generation numbers: 100% (12/12), done.
    	BUG: commit-graph.c:886: missing parent <hash1> for commit <hash2>
    	Aborted (core dumped)
    
    As an initial fix, I converted the code in builtin/fetch.c that calls
    write_commit_graph_reachable() to instead launch a "git commit-graph
    write --reachable --split" process. That code worked, but is not how we
    want the feature to work long-term.
    
    That test did demonstrate that the issue must be something to do with
    internal state of the 'git fetch' process.
    
    The write_commit_graph() method in commit-graph.c ensures the commits we
    plan to write are "closed under reachability" using close_reachable().
    This method walks from the input commits, and uses the UNINTERESTING
    flag to mark which commits have already been visited. This allows the
    walk to take O(N) time, where N is the number of commits, instead of
    O(P) time, where P is the number of paths. (The number of paths can be
    exponential in the number of commits.)
    
    However, the UNINTERESTING flag is used in lots of places in the
    codebase. This flag usually means some barrier to stop a commit walk,
    such as in revision-walking to compare histories. It is not often
    cleared after the walk completes because the starting points of those
    walks do not have the UNINTERESTING flag, and clear_commit_marks() would
    stop immediately.
    
    This is happening during a 'git fetch' call with a remote. The fetch
    negotiation is comparing the remote refs with the local refs and marking
    some commits as UNINTERESTING.
    
    You may ask: did this feature ever work at all? Yes, it did, as long as
    you had a commit-graph covering all of your local refs. My testing was
    unfortunately limited to this scenario. The UNINTERESTING commits are
    always part of the "old" commit-graph, and when we add new commits to a
    top layer of the commit-graph chain those are not needed. If we happen
    to merge layers of the chain, then the commits are added as a list, not
    using a commit walk. Further, the test added for this config option in
    t5510-fetch.sh uses local filesystem clones, which somehow avoids this
    logic.
    
    I tested running clear_commit_marks_many() to clear the UNINTERESTING
    flag inside close_reachable(), but the tips did not have the flag, so
    that did nothing.
    
    It turns out that the calculate_changed_submodule_paths() method is at
    fault. Thanks, Peff, for pointing out this detail! More specifically,
    for each submodule, the collect_changed_submodules() runs a revision
    walk to essentially do file-history on the list of submodules. That
    revision walk marks commits UNININTERESTING if they are simiplified away
    by not changing the submodule.
    
    Instead, I finally arrived on the conclusion that I should use a flag
    that is not used in any other part of the code. In commit-reach.c, a
    number of flags were defined for commit walk algorithms. The REACHABLE
    flag seemed like it made the most sense, and it seems it was not
    actually used in the file. The REACHABLE flag was used in early versions
    of commit-reach.c, but was removed by 4fbcca4 (commit-reach: make
    can_all_from_reach... linear, 2018-07-20).
    
    Add the REACHABLE flag to commit-graph.c and use it instead of
    UNINTERESTING in close_reachable(). This fixes the bug in manual
    testing.
    
    Reported-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Helped-by: Jeff King <peff@peff.net>
    Helped-by: Szeder Gábor <szeder.dev@gmail.com>
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- t/t5510-fetch.sh
+++ t/t5510-fetch.sh
@@ -583,7 +583,7 @@ test_expect_success 'fetch.writeCommitGraph' '
 	)
 '
 
-test_expect_failure 'fetch.writeCommitGraph with submodules' '
+test_expect_success 'fetch.writeCommitGraph with submodules' '
 	pwd="$(pwd)" &&
 	git clone dups super &&
 	(
