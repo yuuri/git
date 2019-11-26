9af2de98dec456f9314121fc964df037be7f5b36
Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>
2019-11-26 11:19:14 +0100

    checkout, restore: support the --pathspec-from-file option
    Decisions taken for simplicity:
    1) For now, `--pathspec-from-file` is declared incompatible with
       `--patch`, even when <file> is not `stdin`. Such use case it not
       really expected.
    2) It is not allowed to pass pathspec in both args and file.
    
    `you must specify path(s) to restore` block was moved down to be able to
    test for `pathspec.nr` instead, because testing for `argc` is no longer
    correct.
    
    `git switch` does not support the new options because it doesn't expect
    `<pathspec>` arguments.
    
    Signed-off-by: Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>

---
+++ t/t2026-checkout-pathspec-file.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+test_description='checkout --pathspec-from-file'
+
+. ./test-lib.sh
+
+test_tick
+
+test_expect_success setup '
+	test_commit file0 &&
+
+	echo 1 >fileA.t &&
+	echo 1 >fileB.t &&
+	echo 1 >fileC.t &&
+	echo 1 >fileD.t &&
+	git add fileA.t fileB.t fileC.t fileD.t &&
+	git commit -m "files 1" &&
+
+	echo 2 >fileA.t &&
+	echo 2 >fileB.t &&
+	echo 2 >fileC.t &&
+	echo 2 >fileD.t &&
+	git add fileA.t fileB.t fileC.t fileD.t &&
+	git commit -m "files 2" &&
+
+	git tag checkpoint
+'
+
+restore_checkpoint () {
+	git reset --hard checkpoint
+}
+
+verify_expect () {
+	git status --porcelain --untracked-files=no -- fileA.t fileB.t fileC.t fileD.t >actual &&
+	test_cmp expect actual
+}
+
+test_expect_success 'simplest' '
+	restore_checkpoint &&
+
+	echo fileA.t | git checkout --pathspec-from-file=- HEAD^1 &&
+
+	cat >expect <<-\EOF &&
+	M  fileA.t
+	EOF
+	verify_expect
+'
+
+test_expect_success 'only touches what was listed' '
+	restore_checkpoint &&
+
+	printf "fileB.t\nfileC.t\n" | git checkout --pathspec-from-file=- HEAD^1 &&
+
+	cat >expect <<-\EOF &&
+	M  fileB.t
+	M  fileC.t
+	EOF
+	verify_expect
+'
+
+test_done
