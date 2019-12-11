bfdd7a4b5678a7d88c048364db5a5654b1852ca5
Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>
2019-12-11 17:36:11 +0100

    t7107, t7526: directly test parse_pathspec_file()
    In my previous patches, `parse_pathspec_file()` was tested indirectly by
    invoking `git reset` and `git commit` with properly crafted inputs. This
    has some disadvantages:
    1) A number of tests are copy&pasted for every command where
       `--pathspec-from-file` is supported. With just two commands, it
       wasn't too bad, but I'm going to extend support to many more
       commands, which would make a handful of low-value tests.
    2) Tests are located in suboptimal test packages
    3) Tests are indirect
    
    Fix this by testing `parse_pathspec_file()` directly via a new test
    helper.
    
    While working on it, I also noticed that quotes testing via
    `"\"file\\101.t\""` was somewhat incorrect: I escaped `\` one time while
    I had to escape it two times! Tests still worked due to `"` which
    prevented pathspec from matching files.
    
    Fix this by properly escaping one more time.
    
    Signed-off-by: Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>

--- t/t7107-reset-pathspec-file.sh
+++ t/t7107-reset-pathspec-file.sh
@@ -25,7 +25,7 @@ verify_expect () {
 	test_cmp expect actual
 }
 
-test_expect_success '--pathspec-from-file from stdin' '
+test_expect_success 'simplest' '
 	restore_checkpoint &&
 
 	git rm fileA.t &&
@@ -35,106 +35,6 @@ test_expect_success '--pathspec-from-file from stdin' '
 	 D fileA.t
 	EOF
 	verify_expect
-'
-
-test_expect_success '--pathspec-from-file from file' '
-	restore_checkpoint &&
-
-	git rm fileA.t &&
-	echo fileA.t >list &&
-	git reset --pathspec-from-file=list &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'NUL delimiters' '
-	restore_checkpoint &&
-
-	git rm fileA.t fileB.t &&
-	printf "fileA.t\0fileB.t\0" | git reset --pathspec-from-file=- --pathspec-file-nul &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	 D fileB.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'LF delimiters' '
-	restore_checkpoint &&
-
-	git rm fileA.t fileB.t &&
-	printf "fileA.t\nfileB.t\n" | git reset --pathspec-from-file=- &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	 D fileB.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'no trailing delimiter' '
-	restore_checkpoint &&
-
-	git rm fileA.t fileB.t &&
-	printf "fileA.t\nfileB.t" | git reset --pathspec-from-file=- &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	 D fileB.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'CRLF delimiters' '
-	restore_checkpoint &&
-
-	git rm fileA.t fileB.t &&
-	printf "fileA.t\r\nfileB.t\r\n" | git reset --pathspec-from-file=- &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	 D fileB.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'quotes' '
-	restore_checkpoint &&
-
-	git rm fileA.t &&
-	printf "\"file\\101.t\"" | git reset --pathspec-from-file=- &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	EOF
-	verify_expect
-'
-
-test_expect_success 'quotes not compatible with --pathspec-file-nul' '
-	restore_checkpoint &&
-
-	git rm fileA.t &&
-	printf "\"file\\101.t\"" >list &&
-	# Note: "git reset" has not yet learned to fail on wrong pathspecs
-	git reset --pathspec-from-file=list --pathspec-file-nul &&
-
-	cat >expect <<-\EOF &&
-	 D fileA.t
-	EOF
-	test_must_fail verify_expect
-'
-
-test_expect_success '--pathspec-from-file is not compatible with --soft or --hard' '
-	restore_checkpoint &&
-
-	git rm fileA.t &&
-	echo fileA.t >list &&
-	test_must_fail git reset --soft --pathspec-from-file=list &&
-	test_must_fail git reset --hard --pathspec-from-file=list
 '
 
 test_expect_success 'only touches what was listed' '
@@ -150,6 +50,15 @@ test_expect_success 'only touches what was listed' '
 	D  fileD.t
 	EOF
 	verify_expect
+'
+
+test_expect_success '--pathspec-from-file is not compatible with --soft or --hard' '
+	restore_checkpoint &&
+
+	git rm fileA.t &&
+	echo fileA.t >list &&
+	test_must_fail git reset --soft --pathspec-from-file=list &&
+	test_must_fail git reset --hard --pathspec-from-file=list
 '
 
 test_done
