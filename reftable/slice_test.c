0bc28ac610fb6f5965e39f9e0d4caf88ca9ead06
Han-Wen Nienhuys <hanwen@google.com>
2020-01-23 17:51:48 +0100

    Add reftable library
    Reftable is a format for storing the ref database. Its rationale and
    specification is in the preceding commit.
    
    This imports the upstream library as one big commit. For understanding
    the code, it is suggested to read the code in the following order:
    
    * The specification under Documentation/technical/reftable.txt
    
    * reftable.h - the public API
    
    * record.{c,h} - reading and writing records
    
    * block.{c,h} - reading and writing blocks.
    
    * writer.{c,h} - writing a complete reftable file.
    
    * merged.{c,h} and pq.{c,h} - reading a stack of reftables
    
    * stack.{c,h} - writing and compacting stacks of reftable on the
    filesystem.
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>

---
+++ reftable/slice_test.c
@@ -0,0 +1,40 @@
+/*
+Copyright 2020 Google LLC
+
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file or at
+https://developers.google.com/open-source/licenses/bsd
+*/
+
+#include "slice.h"
+
+#include "system.h"
+
+#include "basics.h"
+#include "record.h"
+#include "reftable.h"
+#include "test_framework.h"
+#include "reftable-tests.h"
+
+static void test_slice(void)
+{
+	struct slice s = SLICE_INIT;
+	struct slice t = SLICE_INIT;
+
+	slice_set_string(&s, "abc");
+	assert(0 == strcmp("abc", slice_as_string(&s)));
+
+	slice_set_string(&t, "pqr");
+
+	slice_addbuf(&s, &t);
+	assert(0 == strcmp("abcpqr", slice_as_string(&s)));
+
+	slice_release(&s);
+	slice_release(&t);
+}
+
+int slice_test_main(int argc, const char *argv[])
+{
+	add_test_case("test_slice", &test_slice);
+	return test_main(argc, argv);
+}
