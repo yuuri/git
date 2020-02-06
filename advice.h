080d12b5c696d61ac1052d5863f7260a4005346e
Heba Waly <heba.waly@gmail.com>
2020-02-06 13:43:55 +1300

    advice: refactor advise API
    Currently it's very easy for the advice library's callers to miss
    checking the visibility step before printing an advice. Also, it makes
    more sense for this step to be handled by the advice library.
    
    Add a new advise_if_enabled function that checks the visibility of
    advice messages before printing.
    
    Add a new helper advise_enabled to check the visibility of the advice
    if the caller needs to carry out complicated processing based on that
    value.
    
    A list of config variables 'advice_config_keys' is added to be used by
    list_config_advices() instead of 'advice_config[]' because we'll get
    rid of 'advice_config[]' and the global variables once we migrate all
    the callers to use the new APIs.
    
    Also change the advise call in tag library from advise() to
    advise_if_enabled() to construct an example of the usage of the new
    API.
    
    Signed-off-by: Heba Waly <heba.waly@gmail.com>

--- advice.h
+++ advice.h
@@ -29,12 +29,68 @@ extern int advice_ignored_hook;
 extern int advice_waiting_for_editor;
 extern int advice_graft_file_deprecated;
 extern int advice_checkout_ambiguous_remote_branch_name;
-extern int advice_nested_tag;
 extern int advice_submodule_alternate_error_strategy_die;
+
+/**
+ To add a new advice, you need to:
+ - Define an advice_type.
+ - Add a new entry to advice_config_keys list.
+ - Add the new config variable to Documentation/config/advice.txt.
+ - Call advise_if_enabled to print your advice.
+ */
+enum advice_type {
+	FETCH_SHOW_FORCED_UPDATES = 0,
+	PUSH_UPDATE_REJECTED = 1,
+	PUSH_UPDATE_REJECTED_ALIAS = 2,
+	PUSH_NON_FF_CURRENT = 3,
+	PUSH_NON_FF_MATCHING = 4,
+	PUSH_ALREADY_EXISTS = 5,
+	PUSH_FETCH_FIRST = 6,
+	PUSH_NEEDS_FORCE = 7,
+	PUSH_UNQUALIFIED_REF_NAME = 8,
+	STATUS_HINTS = 9,
+	STATUS_U_OPTION = 10,
+	STATUS_AHEAD_BEHIND_WARNING = 11,
+	COMMIT_BEFORE_MERGE = 12,
+	RESET_QUIET_WARNING = 13,
+	RESOLVE_CONFLICT = 14,
+	SEQUENCER_IN_USE = 15,
+	IMPLICIT_IDENTITY = 16,
+	DETACHED_HEAD = 17,
+	SET_UPSTREAM_FAILURE = 18,
+	OBJECT_NAME_WARNING = 19,
+	AMWORKDIR = 20,
+	RM_HINTS = 21,
+	ADD_EMBEDDED_REPO = 22,
+	IGNORED_HOOK = 23,
+	WAITING_FOR_EDITOR = 24,
+	GRAFT_FILE_DEPRECATED = 25,
+	CHECKOUT_AMBIGUOUS_REMOTE_BRANCH_NAME = 26,
+	NESTED_TAG = 27,
+	SUBMODULE_ALTERNATE_ERROR_STRATEGY_DIE = 28,
+};
+
 
 int git_default_advice_config(const char *var, const char *value);
 __attribute__((format (printf, 1, 2)))
 void advise(const char *advice, ...);
+
+/**
+ Checks if advice type is enabled (can be printed to the user).
+ Should be called before advise().
+ */
+int advice_enabled(enum advice_type type);
+
+/**
+ Checks if PUSH_UPDATE_REJECTED advice type is enabled.
+ */
+int advice_push_update_rejected_enabled(void);
+
+/**
+ Checks the visibility of the advice before priniting.
+ */
+void advise_if_enabled(enum advice_type type, const char *advice, ...);
+
 int error_resolve_conflict(const char *me);
 void NORETURN die_resolve_conflict(const char *me);
 void NORETURN die_conclude_merge(void);
