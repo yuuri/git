0767c8b77c820cfc03bbc617da4dc9f20ba4a46a
Johannes Schindelin <johannes.schindelin@gmx.de>
2020-03-24 23:53:32 +0100

    tests: do not let lazy prereqs inside `test_expect_*` turn off tracing
    The `test_expect_*` functions use `test_eval_` and so does
    `test_run_lazy_prereq_`. If tracing is enabled via the `-x` option,
    `test_eval_` turns on tracing while evaluating the code block, and turns
    it off directly after it.
    
    This is unwanted for nested invocations.
    
    One somewhat surprising example of this is when running a test that
    calls `test_i18ngrep`: that function requires the `C_LOCALE_OUTPUT`
    prereq, and that prereq is a lazy one, so it is evaluated via
    `test_eval_`, the command tracing is turned off, and the test case
    continues to run _without tracing the commands_.
    
    Another somewhat surprising example is when one lazy prereq depends on
    another lazy prereq: the former will call `test_have_prereq` with the
    latter one, which in turn calls `test_eval_` and -- you guessed it --
    tracing (if enabled) will be turned off _before_ returning to evaluating
    the other lazy prereq.
    
    As we will introduce just such a scenario with the GPG, GPGSM and
    RFC1991 prereqs, let's fix that by introducing a variable that keeps
    track of the current trace level: nested `test_eval_` calls will
    increment and then decrement the level, and only when it reaches 0, the
    tracing will _actually_ be turned off.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -882,6 +882,7 @@ maybe_setup_valgrind () {
 	fi
 }
 
+_trace_level=0
 want_trace () {
 	test "$trace" = t && {
 		test "$verbose" = t || test "$verbose_log" = t
@@ -895,7 +896,7 @@ want_trace () {
 test_eval_inner_ () {
 	# Do not add anything extra (including LF) after '$*'
 	eval "
-		want_trace && set -x
+		want_trace && _trace_level=$(($_trace_level+1)) && set -x
 		$*"
 }
 
@@ -926,7 +927,8 @@ test_eval_ () {
 		test_eval_ret_=$?
 		if want_trace
 		then
-			set +x
+			test 1 = $_trace_level && set +x
+			_trace_level=$(($_trace_level-1))
 		fi
 	} 2>/dev/null 4>&2
 
