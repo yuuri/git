479fe884e98f77cc82470c56b71fac4603cd6ed6
Han-Wen Nienhuys <hanwen@google.com>
2020-04-23 20:43:12 +0200

    Reftable support for git-core
    For background, see the previous commit introducing the library.
    
    This introduces the refs/reftable-backend.c containing reftable powered ref
    storage backend.
    
    It can be activated by passing --ref-storage=reftable to "git init".
    
    Example use: see t/t0031-reftable.sh
    
    Signed-off-by: Han-Wen Nienhuys <hanwen@google.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Co-authored-by: Jeff King <peff@peff.net>

--- reftable/update.sh
+++ reftable/update.sh
@@ -16,7 +16,4 @@ cp reftable-repo/LICENSE reftable/
 git --git-dir reftable-repo/.git show --no-patch --format=oneline HEAD \
   > reftable/VERSION
 
-mv reftable/system.h reftable/system.h~
-sed 's|if REFTABLE_IN_GITCORE|if 1 /* REFTABLE_IN_GITCORE */|'  < reftable/system.h~ > reftable/system.h
-
 git add reftable/*.[ch] reftable/LICENSE reftable/VERSION
