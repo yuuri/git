5f89e0ec9b9c9a02deed5b79b35253e0462dc87c
Derrick Stolee <dstolee@microsoft.com>
2020-06-26 09:58:40 -0400

    maintenance: create basic maintenance runner
    The 'gc' builtin is our current entrypoint for automatically maintaining
    a repository. This one tool does many operations, such as repacking the
    repository, packing refs, and rewriting the commit-graph file. The name
    implies it performs "garbage collection" which means several different
    things, and some users may not want to use this operation that rewrites
    the entire object database.
    
    Create a new 'maintenance' builtin that will become a more general-
    purpose command. To start, it will only support the 'run' subcommand,
    but will later expand to add subcommands for scheduling maintenance in
    the background.
    
    For now, the 'maintenance' builtin is a thin shim over the 'gc' builtin.
    In fact, the only option is the '--auto' toggle, which is handed
    directly to the 'gc' builtin. The current change is isolated to this
    simple operation to prevent more interesting logic from being lost in
    all of the boilerplate of adding a new builtin.
    
    Use existing builtin/gc.c file because we want to share code between the
    two builtins. It is possible that we will have 'maintenance' replace the
    'gc' builtin entirely at some point, leaving 'git gc' as an alias for
    some specific arguments to 'git maintenance run'.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- builtin/gc.c
+++ builtin/gc.c
@@ -704,3 +704,64 @@ int cmd_gc(int argc, const char **argv, const char *prefix)
 
 	return 0;
 }
+
+static const char * const builtin_maintenance_usage[] = {
+	N_("git maintenance run [<options>]"),
+	NULL
+};
+
+struct maintenance_opts {
+	int auto_flag;
+} opts;
+
+static int maintenance_task_gc(struct repository *r)
+{
+	int result;
+	struct argv_array cmd = ARGV_ARRAY_INIT;
+
+	argv_array_pushl(&cmd, "gc", NULL);
+
+	if (opts.auto_flag)
+		argv_array_pushl(&cmd, "--auto", NULL);
+
+	close_object_store(r->objects);
+	result = run_command_v_opt(cmd.argv, RUN_GIT_CMD);
+	argv_array_clear(&cmd);
+
+	return result;
+}
+
+static int maintenance_run(struct repository *r)
+{
+	return maintenance_task_gc(r);
+}
+
+int cmd_maintenance(int argc, const char **argv, const char *prefix)
+{
+	struct repository *r = the_repository;
+
+	static struct option builtin_maintenance_options[] = {
+		OPT_BOOL(0, "auto", &opts.auto_flag,
+			 N_("run tasks based on the state of the repository")),
+		OPT_END()
+	};
+
+	memset(&opts, 0, sizeof(opts));
+
+	if (argc == 2 && !strcmp(argv[1], "-h"))
+		usage_with_options(builtin_maintenance_usage,
+				   builtin_maintenance_options);
+
+	argc = parse_options(argc, argv, prefix,
+			     builtin_maintenance_options,
+			     builtin_maintenance_usage,
+			     PARSE_OPT_KEEP_UNKNOWN);
+
+	if (argc == 1) {
+		if (!strcmp(argv[0], "run"))
+			return maintenance_run(r);
+	}
+
+	usage_with_options(builtin_maintenance_usage,
+			   builtin_maintenance_options);
+}
