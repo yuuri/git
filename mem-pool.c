eae8b2923f7834f3b1d030f6eb4dc093b94da91b
Elijah Newren <newren@gmail.com>
2020-07-22 11:23:07 -0700

    mem-pool: add convenience functions for xstrdup and xstrndup
    fast-import had a special mem_pool_xstrdup() convenience function that I
    want to be able to use from the new merge algorithm I am writing.  Move
    it from fast-import to mem-pool, and also add a mem_pool_xstrndup()
    while at it that I also want to use.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>

--- mem-pool.c
+++ mem-pool.c
@@ -102,6 +102,29 @@ void *mem_pool_calloc(struct mem_pool *mem_pool, size_t count, size_t size)
 	return r;
 }
 
+char *mem_pool_xstrdup(struct mem_pool *pool, const char *str)
+{
+	size_t len = strlen(str) + 1;
+	char *ret = mem_pool_alloc(pool, len);
+
+	if (!ret)
+		die("Out of memory, mem_pool_xstrdup failed");
+
+	return memcpy(ret, str, len);
+}
+
+char *mem_pool_xstrndup(struct mem_pool *pool, const char *str, size_t len)
+{
+	size_t minlen = strnlen(str, len);
+	char *ret = mem_pool_alloc(pool, minlen+1);
+
+	if (!ret)
+		die("Out of memory, mem_pool_xstrndup failed");
+
+	ret[minlen] = '\0';
+	return memcpy(ret, str, minlen);
+}
+
 int mem_pool_contains(struct mem_pool *mem_pool, void *mem)
 {
 	struct mp_block *p;
